f3e7862bcea0b5d9f666e48dec5a3aec60f8a62d
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-03-12 08:14:37 +0000

    /logoutのリダイレクト処理のテストを追加

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
+// 'debug'モジュール呼び出し
+const debug = require('debug');
+// デバッガーを作成する
+const indexJs_debugger = debug('debug:index.js');
+
 var express = require('express');
 var router = express.Router();
 
+indexJs_debugger('index.js開始');
+
 /* GET home page. */
+// GETアクセスが送られてきた場合の処理
 router.get('/', function(req, res, next) {
+  indexJs_debugger('GET処理');
   res.render('index', { title: 'Express' });
 });
 
