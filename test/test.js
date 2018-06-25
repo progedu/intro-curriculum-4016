56216cce115c4384663e30622a7348b33c2ebcfd
Hiroaki Tagawa <h.tagawa@capandlow.com>
2018-06-25 03:16:46 +0000

    ログアウト時のテスト追加

--- test/test.js
+++ test/test.js
@@ -29,3 +29,14 @@ describe('/login', () => {
       .expect(200, done);
   });
 });
+
+describe('/logout', () => {
+
+  it('ログアウトした際にトップページにリダイレクトされる', (done) => {
+    request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302, done);
+  });
+
+});
