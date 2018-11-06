5b635d1455413079f6e4db59b9520c642f7c7391
kansuke224 <kansuke224224@gmail.com>
2018-11-06 02:54:42 +0000

    /logout にアクセスした際に / にリダイレクトされる テストを追加

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ describe('/login', () => {
     passportStub.install(app);
     passportStub.login({ username: 'testuser' });
   });
-  
+
   after(() => {
     passportStub.logout();
     passportStub.uninstall(app);
@@ -29,3 +29,13 @@ describe('/login', () => {
       .expect(200, done);
   });
 });
+
+describe('/logout', () => {
+  it('/ にリダイレクトされる', (done) => {
+    request(app)
+    .get('/logout')
+    .expect('Location', '/')
+    .expect(302, done);
+  });
+
+})
