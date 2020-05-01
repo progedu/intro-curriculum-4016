23baf7190f7323e8beae8b894d95d7f94603607c
kazumaisamatsu <54142720+kazumaisamatsu@users.noreply.github.com>
2020-05-01 18:34:56 +0900

    リダイレクト表示実装

--- test/test.js
+++ test/test.js
@@ -4,15 +4,15 @@ const app = require('../app');
 const passportStub = require('passport-stub');
 
 describe('/login', () => {
-  before(() => {
-    passportStub.install(app);
-    passportStub.login({ username: 'testuser' });
-  });
-  
-  after(() => {
-    passportStub.logout();
-    passportStub.uninstall(app);
-  });
+    before(() => {
+       passportStub.install(app);
+      passportStub.login({ username: 'testuser' });
+    });
+   
+      after(() => {
+        passportStub.logout();
+        passportStub.uninstall(app);
+      });
 
   it('ログインのためのリンクが含まれる', (done) => {
     request(app)
@@ -21,11 +21,23 @@ describe('/login', () => {
       .expect(/<a href="\/auth\/github"/)
       .expect(200, done);
   });
+   it('ログイン時はユーザー名が表示される', (done) => {
+        request(app)
+          .get('/login')
+          .expect(/testuser/)
+          .expect(200, done);
+      }); 
+      
 
-  it('ログイン時はユーザー名が表示される', (done) => {
-    request(app)
-      .get('/login')
-      .expect(/testuser/)
-      .expect(200, done);
-  });
 });
+
+describe('/logout', () => {
+    it('/ にリダイレクトされる', (done) => {
+      request(app)
+        .get('/logout')
+        .expect('Location', '/')
+        .expect(302, done);
+    });
+  });
+  
+  
