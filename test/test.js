d0cc8a7fa90c26dd0266d279138fd7d7a5f93cd1
sanada <gigglingirl1225@gmail.com>
2020-05-05 22:17:03 +0900

    ログアウト時のリダイレクトのテストを追加

--- test/test.js
+++ test/test.js
@@ -26,3 +26,12 @@ describe('/login', () => {
       .expect(200);
   });
 });
+
+describe('/logout', () => {
+  test('ログアウト時にトップページにリダイレクトされる', () => {
+    return request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302);
+  })
+})
