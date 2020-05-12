817f429922d897b552eb0b60f3574a1cb3404e1d
testori-2020-b2u <sypkc746wind@yahoo.co.jp>
2020-05-12 19:09:14 +0900

    ログアウトのリダイレクトテスト処理追加

--- test/test.js
+++ test/test.js
@@ -1,31 +1,35 @@
-'use strict';
-const request = require('supertest');
-const app = require('../app');
-const passportStub = require('passport-stub');
+"use strict";
+const request = require("supertest");
+const app = require("../app");
+const passportStub = require("passport-stub");
 
-describe('/login', () => {
+describe("/login", () => {
   before(() => {
     passportStub.install(app);
-    passportStub.login({ username: 'testuser' });
+    passportStub.login({ username: "testuser" });
   });
-  
+
   after(() => {
     passportStub.logout();
     passportStub.uninstall(app);
   });
 
-  it('ログインのためのリンクが含まれる', (done) => {
+  it("ログインのためのリンクが含まれる", (done) => {
     request(app)
-      .get('/login')
-      .expect('Content-Type', 'text/html; charset=utf-8')
+      .get("/login")
+      .expect("Content-Type", "text/html; charset=utf-8")
       .expect(/<a href="\/auth\/github"/)
       .expect(200, done);
   });
 
-  it('ログイン時はユーザー名が表示される', (done) => {
+  it("ログイン時はユーザー名が表示される", (done) => {
     request(app)
-      .get('/login')
+      .get("/login")
       .expect(/testuser/)
       .expect(200, done);
+  });
+
+  it("/logout にアクセスした際に / にリダイレクトされる", (done) => {
+    request(app).get("/logout").expect("Location", "/").expect(302, done);
   });
 });
