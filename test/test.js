baceab4d743dac2d5c0c72c351a5d6b4bc87b7c6
Ichiya Nakashima <ichinaka3@gmail.com>
2020-05-24 12:10:22 +0900

    add test code for redirect at logout

--- test/test.js
+++ test/test.js
@@ -26,3 +26,13 @@ describe('/login', () => {
       .expect(200);
   });
 });
+
+describe('/logout', () => {
+  test('/ へのリダイレクトの確認', () => {
+    return request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302);
+
+  });
+});
