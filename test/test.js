a816d8ebb9a350293cba30990c46d7da80757ce4
ta-nu-ki3 <unplugged61662@gmail.com>
2020-11-25 18:05:11 +0900

    練習問題の答

--- test/test.js
+++ test/test.js
@@ -25,4 +25,10 @@ describe('/login', () => {
       .expect(/testuser/)
       .expect(200);
   });
+  test('/logoutにアクセスした際に/にリダイレクトされる', () => {
+    return request(app)
+    .get('/logout')
+    .expect('Location', '/')
+    .expect(302)
+  })
 });
