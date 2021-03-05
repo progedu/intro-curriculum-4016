8cc8446c53be25a2317165078459deb9a101b615
natuume <nonegemu3e@gmail.com>
2021-03-05 20:33:49 +0900

    /logout にアクセスした際に / にリダイレクトされるテストを追加

--- test/test.js
+++ test/test.js
@@ -3,26 +3,36 @@ const request = require('supertest');
 const app = require('../app');
 const passportStub = require('passport-stub');
 
-describe('/login', () => {
+describe('/login', () => {//ログイン時のテスト
   beforeAll(() => {
     passportStub.install(app);
-    passportStub.login({ username: 'testuser' });
+    passportStub.login({ username: 'testuser' });//testuserでログイン
   });
   afterAll(() => {
     passportStub.logout();
     passportStub.uninstall(app);
   });
-  test('ログインのためのリンクが含まれる', () => {
+  test('ログインのためのリンクが含まれる', () => {//ログイン時のリンクテスト
     return request(app)
       .get('/login')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(/<a href="\/auth\/github"/)
-      .expect(200);
+      .expect('Content-Type', 'text/html; charset=utf-8')//htmlのヘッダーがあるか？
+      .expect(/<a href="\/auth\/github"/)//Github認証のURLあるか
+      .expect(200);//200で通るか
   });
   test('ログイン時はユーザー名が表示される', () => {
     return request(app)
       .get('/login')
-      .expect(/testuser/)
-      .expect(200);
+      .expect(/testuser/)//testuserが表示されているか
+      .expect(200);//200で通っているか
   });
+
+  describe('/logout', () => {
+    test('/ にリダイレクトされる', () => {
+      return request(app)
+        .get('/logout')
+        .expect('Location', '/')// リダイレクト先は正しいか
+        .expect(302);//302になっているか
+    });
+  });
+
 });
