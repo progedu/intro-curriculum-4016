2f103b4e99461415de4943ffeebc1b21f11848b3
Hiroshi Shibata <shibata.h.460@gmail.com>
2021-07-29 18:13:55 +0900

    ログアウト時に、/ にリダイレクトされる仕様のテストを実装

--- test/test.js
+++ test/test.js
@@ -26,3 +26,11 @@ describe('/login', () => {
       .expect(200);
   });
 });
+describe('/logout', () =>{
+    test('ログアウト時に、/ にリダイレクトされる', () => {
+      return request(app)
+        .get('/logout')
+        .expect('Location', '/')
+        .expect(302);
+    });
+});
