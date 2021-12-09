d69f122f974ad4ca6ef232c09cedde7d8e4afc72
kahunman <sugi.kahun155@gmail.com>
2021-12-09 17:50:58 +0900

    ログアウト時/ にリダイレクトされるテストを追加

--- test/test.js
+++ test/test.js
@@ -26,3 +26,11 @@ describe('/login', () => {
       .expect(200);
   });
 });
+describe('/logout', () => {
+  test('ログアウト時は/ にリダイレクトされる', () => {
+    return request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302);
+  });
+});
